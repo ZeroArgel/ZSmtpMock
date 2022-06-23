9bd8ef8628e4542f7706ce9d6f649e914db03d2f
ZeroArgek <kevinhumberto9@hotmail.com>
2022-06-23 18:54:35 -0500

    save changes

---
+++ ZSmtpMock/IZSmtpMock.cs
@@ -0,0 +1,14 @@
+﻿namespace ZSmtpMock
+{
+    using System.Net.Mail;
+    public interface IZSmtpMock
+    {
+        void Send(MailMessage mailMessage);
+
+        enum Port
+        {
+            P25 = 25,
+            P587 = 587
+        }
+    }
+}
---
+++ ZSmtpMock/ZSmtpMock.cs
@@ -0,0 +1,25 @@
+﻿namespace ZSmtpMock
+{
+    using System.Net.Mail;
+    public class ZSmtpMock : IZSmtpMock
+    {
+        public SmtpClient SmtpClient { get; set; }
+        public ZSmtpMock(string host, Port port)
+        {
+            SmtpClient = new SmtpClient(host, (int)port);
+        }
+        public ZSmtpMock(string host, int port)
+        {
+            SmtpClient = new SmtpClient(host, port);
+        }
+        public void Send(MailMessage mailMessage)
+        {
+            SmtpClient.Send(mailMessage);
+        }
+        public enum Port
+        {
+            P25 = 25,
+            P587 = 587
+        }
+    }
+}
